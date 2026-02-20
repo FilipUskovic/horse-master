8db0e11fd7b7dd70eed415916d72b85b7154e90a
Filip Uskovic <filip.uskovic@outlook.com>
2026-02-20 23:01:34 +0100

    added cronjobs key

--- src/routes/api/keep-alive/+server.ts
+++ src/routes/api/keep-alive/+server.ts
@@ -1,17 +1,17 @@
 import { json, error } from '@sveltejs/kit';
 import { supabase } from '$lib/supabase';
-//import { CRON_SECRET } from '$env/static/private';
+import { CRON_SECRET } from '$env/static/private';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ request }) => {
     //  (Authorization Bearer)
     const authHeader = request.headers.get('authorization');
     
-    /*
+    
     if (authHeader !== `Bearer ${CRON_SECRET}`) {
         throw error(401, 'Unauthorized - Pristup zabranjen');
     }
-        */
+        
 
 
     //  RPC pinga
